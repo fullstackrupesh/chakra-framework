794a4431d22a35eb1aaf96b2e5bd8e7283ad1d84
Rupesh Telang <telang_rupesh@live.in>
2018-03-31 23:39:26 +0530

    Chakra.Core class library added.

---
+++ Chakra/Core/Models/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chakra.Core.Models
+{
+    [Serializable]
+    public class Address
+    {
+        [StringLength(255)]
+        public string Street { get; set; }
+        [StringLength(50)]
+        public string City { get; set; }
+        [StringLength(50)]
+        public string State { get; set; }
+        [StringLength(50)]
+        public string Country { get; set; }
+        [StringLength(10)]
+        public string Zip { get; set; }
+    }
+}
