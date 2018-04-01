b8deaa8898a204f9fe499ff9ab28e8e3c7ff6025
Rupesh Telang <telang_rupesh@live.in>
2018-04-02 00:14:23 +0530

    ChakraRepository class template added.

---
+++ Chakra/Core.Data/ChakraRepository.cs
@@ -0,0 +1,34 @@
+﻿using Chakra.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chakra.Core.Data
+{
+    public class ChakraRepository<T, TId> : IChakraRepository<T, TId>
+        where T : class, new()
+        where TId : struct
+    {
+        public void Delete(TId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TId Insert(T record)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T record)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Chakra/Core.Interfaces/IChakraRepository.cs
+++ Chakra/Core.Interfaces/IChakraRepository.cs
@@ -3,8 +3,6 @@
 namespace Chakra.Core.Interfaces
 {
     public interface IChakraRepository<T, TId>
-        where T : class, new()
-        where TId : struct
     {
         TId Insert(T record);
         IEnumerable<T> Get();
