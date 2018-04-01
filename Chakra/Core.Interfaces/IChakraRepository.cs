cc3f560552fd1ad790b2adc768f9ae89d039f228
Rupesh Telang <telang_rupesh@live.in>
2018-04-01 18:50:22 +0530

    ChakraController Added with IChakraRepository skeleton.

---
+++ Chakra/Core.Interfaces/IChakraRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Chakra.Core.Interfaces
+{
+    public interface IChakraRepository<T, TId>
+        where T : class, new()
+        where TId : struct
+    {
+        TId Insert(T record);
+        IEnumerable<T> Get();
+        void Update(T record);
+        void Delete(TId id);
+    }
+}
---
+++ Chakra/Core/ChakraController.cs
@@ -0,0 +1,28 @@
+﻿using Chakra.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Chakra.Core
+{
+    public class ChakraController<T, TId> : Controller
+        where T : class, new()
+        where TId : struct
+    {
+        protected IChakraRepository<T, TId> Repository { get; set; }
+
+        public ChakraController()
+        {
+
+        }
+
+        public virtual ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
