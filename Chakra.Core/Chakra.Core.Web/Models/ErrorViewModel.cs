024654e33595b9583f2e28b43de7ceb9f570745c
Rupesh Telang <rupesh.telang@outlook.com>
2019-06-06 23:57:23 +0530

    ASP.NET CORE Initial Commit

---
+++ Chakra.Core/Chakra.Core.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Chakra.Core.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
